fc98f22b7b2069cee5991ce4c53f2c37c3b057c7
Julien Bidoret – accentgrave <julien@accentgrave.net>
2020-08-10 10:48:36 +0200

    Go gulp

---
+++ README.md
@@ -0,0 +1,49 @@
+# Kirby Basic Dev Kit
+
+Based on (kirby-devkit)[https://github.com/julien-gargot/kirby-devkit] by Julien Gargot + Louis Eveillard. Updated to Gulp 4. Added [cachebuster](https://github.com/schnti/kirby3-cachebuster.git),
+[markdown-field](https://github.com/sylvainjule/kirby-markdown-field) and [kirby3-feed](https://github.com/bnomei/kirby3-feed.git) plugins to submodules.
+
+
+# Kirby Dev Kit
+
+This repo is a starting point to use Kirby 3 with Gulp and Npm. It is made for developers to bootstrap their own projects quickly.
+
+## Requirements
+
+You will need Npm and GIT (for the Kirby submodules) to use this project.
+
+## Setup a new project
+
+1. clone this repo:
+  ```
+  git clone https://github.com/jbidoret/kirby-basic-devkit.git your-project
+  cd your-project
+  git submodule update --init
+  cd kirby
+  composer update
+  ```
+
+2. install Npm from projct root:
+  ```
+  npm install
+  ```
+
+3. to compile all files, for **development** and **production** :
+  ```
+  gulp
+  ```
+
+  To make it faster, while developing, you can watch for changes to CSS and JS files in the assets folder. This task only compiles **development** files.
+  ```
+  gulp dev
+  ```
+
+  Same as `gulp dev` with live reload.
+  ```
+  gulp sync
+  ```
+
+## Configure with your server/site settings
+
+1. rename the file in `site/config/config.localhost.php` to your local development site URL [check out Multi-environment setup](http://getkirby.com/docs/advanced/options). The `environment` variable is used to load minified or unminified CSS/JS versions (checkout `snippets/header.php` and `snippets/footer.php`).
+2. to be able to use browser sync (live reloading, remote debugging, and a few other nice features), set the `localDevUrl` variable to the URL of your site in `assets/manifest.js`.
---
+++ gulpfile.js
@@ -0,0 +1,206 @@
+/*
+
+---------------------------------------
+User settings
+---------------------------------------
+
+User settings are set in ./assets/manifest.js
+
+*/
+
+
+
+/*
+
+---------------------------------------
+Gulp definition
+---------------------------------------
+
+By default you don’t have to configure anything to
+make Gulp work.
+
+- gulp `default` is `build`.
+
+- gulp `build` runs everything to create all files needed in production.
+  The task performs the following instructions: “kirby”, “lint”,
+  “sass”, “css” and “scripts”.
+
+- gulp `dev` runs only the essentials to create all files needed for
+  development. The task performs the following instructions:
+  “kirby”, “lint” and “sass”.
+
+- gulp `sync` task executes “kirby”,
+  run “lint”, “scripts” and “sass” watching `assets/manifest.js`,
+  run “lint” watching `userScripts` and
+  run “sass” watching `userStyles`.
+
+*/
+
+
+
+// Helpers function
+Array.prototype.unique = function() {
+  return this.reduce(function(accum, current) {
+    if (accum.indexOf(current) < 0) {
+      accum.push(current);
+    }
+    return accum;
+  }, []);
+}
+
+
+
+// Global variables
+var vars = require("./assets/manifest.js");
+
+// Include gulp
+const { series, src, dest, watch } = require('gulp');
+
+// Include Our Plugins
+var gp_autoprefixer = require('gulp-autoprefixer');
+var gp_browserSync  = require('browser-sync').create();
+var gp_concat       = require('gulp-concat');
+var gp_concatCss    = require('gulp-concat-css');
+var gp_fs           = require('fs');
+var gp_jshint       = require('gulp-jshint');
+var gp_nano         = require('gulp-cssnano');
+var gp_plumber      = require('gulp-plumber');
+var gp_rename       = require('gulp-rename');
+var gp_sass         = require('gulp-sass');
+var gp_uglify       = require('gulp-uglify');
+
+
+// Compile our SCSS
+function sass() {
+  return src( vars.userStyles )
+    .pipe(gp_sass())
+    .pipe(gp_autoprefixer({
+    	cascade: false
+    }))
+    .pipe(dest('assets/css'))
+    .pipe(gp_browserSync.stream());   
+}
+
+
+// Prefix & Minify CSS
+function css(cb) {
+
+  // get CSS (ordered)
+  var styles = vars.userStyles.map(function(path){
+    return path.replace(
+      /^((.+)\/)?((.+?)(\.[^.]*$|$))$/g,
+      "assets/css/$4.css"
+    );
+  });
+  styles = vars.pluginStyles.concat( styles );
+
+  return src( styles, { base: 'assets/production' } )
+    .pipe(gp_concatCss('all.min.css', {
+      inlineImports: false,
+      rebaseUrls: true
+    }))
+    .pipe(dest('assets/production'))
+    .pipe(gp_nano({discardComments: {removeAll: true}, autoprefixer: false}))
+    .pipe(dest('assets/production'));
+}
+
+
+// Lint Task
+function lint() {
+  return src( vars.userScripts )
+    .pipe(gp_jshint())
+    .pipe(gp_jshint.reporter('default'));
+}
+
+
+// Concatenate JS plugins with user scripts and minify them.
+function scripts(cb) {
+  var scripts = vars.pluginScripts.concat( vars.userScripts );
+  return src( scripts )
+    .pipe(gp_concat('all.min.js'))
+    .pipe(gp_uglify({
+      compress: {
+        drop_debugger : true,
+        drop_console : true
+      }
+    }))
+    .pipe(dest('assets/production'));
+}
+
+
+
+// Export `manifest.js` vars to PHP through a kirby plugin.
+// cf. `snippets/header` and `snippets/footer`
+function kirby(done){
+  // get CSS (ordered)
+  var styles = vars.userStyles.map(function(path){
+    return path.replace(
+      /^((.+)\/)?((.+?)(\.[^.]*$|$))$/g,
+      "assets/css/$4.css"
+    );
+  });
+  styles = vars.pluginStyles.concat( styles );
+
+  // get JS
+  var scripts = vars.pluginScripts.concat( vars.userScripts );
+
+  // Create a Kirby plugin that defines asset vars
+  var destination = 'site/plugins/assets';
+  var assets  = "<?php\n";
+      assets += "# Automatically generated file by Gulp for kirby-basic-devkit; DO NOT EDIT.\n";
+      assets += "Kirby::plugin('basic-devkit/assets', [\n";
+      assets += "  'options' => [\n";
+      assets += "    'styles' => " + JSON.stringify(styles) + ",\n";
+      assets += "    'scripts' => " + JSON.stringify(scripts) + ",\n";
+      assets += "  ]\n";
+      assets += "]);";
+
+  if(!gp_fs.existsSync(destination)) {
+    gp_fs.mkdirSync(destination);
+  }
+  gp_fs.writeFileSync(destination + '/index.php', assets );
+  done();
+}
+
+
+// Live reload sync on every screen connect to localhost
+
+function sync(){
+  gp_browserSync.init({
+    proxy: vars.localDevUrl,
+    notify: false,
+    snippetOptions: {
+      ignorePaths: ['panel/**', 'site/accounts/**']
+    },
+  });
+
+  var scripts_folders = vars.userScripts.map(function(path){
+    return path.replace(
+      /^((.+)\/)?((.+?)(\.[^.]*$|$))$/g,
+      "$1**"
+    );
+  }).unique();
+  var styles_folders = vars.userStyles.map(function(path){
+    return path.replace(
+      /^((.+)\/)?((.+?)(\.[^.]*$|$))$/g,
+      "$1**"
+    );
+  }).unique();
+
+  watch( './assets/manifest.js', series(kirby, lint, sass));
+  watch( scripts_folders, series(lint));
+  watch( styles_folders, series(sass));
+
+  // templates & snippets
+  watch(["./site/templates/*.php", "./site/snippets/*.php"], { events: 'change' }, function(done) {
+    gp_browserSync.reload();
+    done();
+  });
+  
+}
+
+
+exports.sync = series(kirby, sync);
+exports.dev = series(kirby, lint, sass);
+exports.build = series(kirby, sass, css, lint, scripts);
+exports.default = series(kirby, sass, css, lint, scripts);
