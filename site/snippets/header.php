de70ff9dec0be9c3b02d7ec3d53357b8eb9a3b60
Julien Bidoret – accentgrave <julien@accentgrave.net>
2021-07-29 09:51:56 +0200

    Better js/no-js script in html header

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -1,11 +1,10 @@
 <!DOCTYPE html>
-<html lang="<?php echo $kirby->language() ?? 'fr' ?>">
+<html lang="<?php echo $kirby->language() ?? 'fr' ?>"  class="no-js">
 <head>
 
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width,initial-scale=1.0">
-  <script>document.getElementsByTagName('html')[0].className = 'js'</script>
-  
+  <script>(function(html){html.className = html.className.replace(/\bno-js\b/,'js')})(document.documentElement);</script>
   <?php snippet("header.metas") ?>
 
   <?php
