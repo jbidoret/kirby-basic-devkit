76e0a931fcf19c9d4367fb4f79bd44641926052b
Julien Bidoret <julien@accentygrave.net>
2023-04-04 11:58:19 +0200

    Update deploy script php 8.2

--- deploy.php
+++ deploy.php
@@ -22,7 +22,7 @@
 	$output = '';
 	foreach($commands AS $command){
 		// Run it
-		$tmp = shell_exec($command);
+		$tmp = shell_exec($command) || "";
 		// Output
 		$output .= "\n<span style=\"color: #6BE234;\">\$</span> <span style=\"color: #000;\">{$command}\n</span>";
 		$output .= htmlentities(trim($tmp)) . "\n";
